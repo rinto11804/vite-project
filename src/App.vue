aed337c775736b322c92d248953a7ba771b9f6da
rinto11804 <rinto11804@gmail.com>
2021-12-18 18:24:07 +0530

    firs comnit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import Model from './components/Model.vue'
+export default {
+   components: {
+      Model
+   },
+   data() {
+      return {
+         header: "Sign up for giveaway",
+         text: "Grap your swag",
+         showModel: false
+      }
+   },
+   methods: {
+      toggleModel() {
+         this.showModel = !this.showModel
+      }
+   }
+}
+</script>
+
+<template>
+   <h1>Hello Vue</h1>
+   <Model v-show="showModel" :header="header" :text="text" theme="sale" @close="toggleModel" />
+   <button @click="toggleModel">openModel</button>
+</template>
+
+<style>
+#app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   text-align: center;
+   color: #243343;
+}
+</style>
